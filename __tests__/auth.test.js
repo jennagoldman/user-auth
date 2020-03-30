7e7517bdeaa628ffbb980e0a2c506ca958e5e0d9
Jenna Goldman <jenna.goldman89@gmail.com>
2020-03-30 15:55:16 -0700

    add login and verify routes and tests, and ensure-auth middleware

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -31,4 +31,32 @@ describe('auth routes', () => {
         });
       });
   });
+  it('logs in a user', async() => {
+    await User.create({ username: 'jennag', password: 'testPassword' });
+
+    request(app)
+      .post('/api/v1/auth/login')
+      .send({ username: 'jennag', password: 'testPassword' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'jennag',
+          __v: 0
+        });
+      });
+  });
+
+  it('fails to login a user with incorrect password', async() => {
+    await User.create({ username: 'jennag', password: 'testPassword' });
+
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ username: 'jennag', password: 'wrongPassword' })
+      .then(res => {
+        expect(res.body).toEqual({
+          message: 'Invalid username or password',
+          status: 403
+        });
+      });
+  });
 });
